3865376a348d80f292b368570e8bdd972e1f622f
Nicolas Avila <avilanicolascgmail.com>
2016-10-10 21:38:00 -0700

    Authentication proof of concept.

---
+++ authenticationTest/src/main/java/com/avilan/tinyfsservice/credentials/ClientCredentialsProvider.java
@@ -0,0 +1,35 @@
+package com.avilan.tinyfsservice.credentials;
+
+import java.util.Set;
+import java.util.UUID;
+import java.util.HashSet;
+
+import com.avilan.tinyfsservice.ClientCredentialsProto.ClientCredentials;
+
+// TODO: Make this class thread safe, find a better way to store known client credentials.
+public class ClientCredentialsProvider {
+    private Set<ClientCredentials> knownClientCredentials;
+
+    public ClientCredentialsProvider() {
+        this.knownClientCredentials = new HashSet<ClientCredentials>();
+    }
+
+    public ClientCredentials provideNewClientCredentials() {
+        ClientCredentials clientCredentials =
+            ClientCredentials.newBuilder()
+                .setClientId(UUID.randomUUID().toString())
+                .build();
+
+        knownClientCredentials.add(clientCredentials);
+
+        return clientCredentials;
+    }
+
+    public void unregisterClient(final ClientCredentials clientCredentials) {
+        knownClientCredentials.remove(clientCredentials);
+    }
+
+    public boolean isKnownClient(final ClientCredentials clientCredentials) {
+        return knownClientCredentials.contains(clientCredentials);
+    }
+}
