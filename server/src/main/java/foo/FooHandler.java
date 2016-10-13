21d567c10180821100309cf8f69c9ca9978bb777
Joseph Arhar <josepharhar@gmail.com>
2016-10-13 14:17:28 -0400

    added two-way websocket support for server

--- server/src/main/java/foo/FooHandler.java
+++ server/src/main/java/foo/FooHandler.java
@@ -1,5 +1,7 @@
 package foo;
 
+import java.io.IOException;
+
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.WebSocketSession;
@@ -7,8 +9,22 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 public class FooHandler extends TextWebSocketHandler {
 
+  private WebSocketSession session;
+
+  @Override
+  public void afterConnectionEstablished(WebSocketSession session) {
+    System.out.println("FooHandler::afterConnectionEstablished");
+    this.session = session;
+  }
+
   @Override
   public void handleTextMessage(WebSocketSession session, TextMessage message) {
-    System.out.println("handleTextMessage \"" + message.getPayload() + "\"");
+    System.out.println("FooHandler::handleTextMessage \"" + message.getPayload() + "\"");
+    System.out.println("this.session == session: " + (this.session == session));
+    try {
+      session.sendMessage(new TextMessage("Hello from Java Spring server!"));
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
   }
 }
