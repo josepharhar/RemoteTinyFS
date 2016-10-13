6bf0f7be20be41e1c4dd3d6b7f4c8387288f64df
Nicolas Avila <avilanicolasc@gmail.com>
2016-10-13 14:37:36 -0700

    Remote authentication proof of concept, unify protobuf code with server

---
+++ server/src/main/java/com/tinyfs/exception/CredentialsException.java
@@ -0,0 +1,11 @@
+package com.tinyfs.exception;
+
+public class CredentialsException extends Exception {
+
+  private static final long serialVersionUID = 1L;
+
+  public CredentialsException(Exception e) {
+    super(e);
+  }
+
+}
