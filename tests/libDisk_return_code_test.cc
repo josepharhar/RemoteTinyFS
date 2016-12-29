5b1d5cb4a25637b0ab06219ab695735ac80b4fac
Joseph Arhar <josepharhar@gmail.com>
2016-12-28 22:53:11 -0800

    testing ??

--- tests/libDisk_return_code_test.cc
+++ tests/libDisk_return_code_test.cc
@@ -1,13 +1,21 @@
 #include "test.h"
 
+#include <string>
+
 int main(int argc, char** argv) {
   char* token = argv[1];
-  int return_code;
+  int return_code, disk;
+
+  disk = openDisk(__FILE__, 0);
+  if (disk < 0) {
+    // disk already exists
+  } else {
+    // disk does not exist
+  }
 
   return_code = initLibDisk(token);
   if (return_code) {
     FAIL("initLibDisk() returned " << return_code);
   }
   INFO("Successfully connected to server");
-
 }
